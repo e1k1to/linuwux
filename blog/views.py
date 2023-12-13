33e6344514a604f327d4182ac89e34018f74d459
e1k1to <eikimillian@protonmail.com>
2023-12-13 03:39:08 -0300

    update no card

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from .models import postzinho
 
 # Create your views here.
 def blog(request):
-  items = postzinho.objects.all()
+  items = reversed(postzinho.objects.all())
   return render(request, "blog.html", {"posts": items });
