b15c816bc9ab8966100169f1a1dc14f7f10803bc
e1k1to <eikimillian@protonmail.com>
2024-05-17 00:39:40 -0300

    a

--- linuwux/settings.py
+++ linuwux/settings.py
@@ -129,7 +129,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 # Default primary key field type
