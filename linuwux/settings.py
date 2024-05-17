240c93d5aaf057c12ebdefa6e39cea0fbed956a2
e1k1to <eikimillian@protonmail.com>
2024-05-17 01:48:11 -0300

    aa

--- linuwux/settings.py
+++ linuwux/settings.py
@@ -125,15 +125,14 @@ USE_TZ = True
 
 #STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
-    'staticfiles',
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 WSGI_APPLICATION = 'linuwux.wsgi.application'
 
