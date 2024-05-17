4ec034c86a049f2373dd28347c861f670788b847
e1k1to <eikimillian@protonmail.com>
2024-05-17 01:02:22 -0300

    a

--- linuwux/settings.py
+++ linuwux/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 #STATIC_URL = 'static/'
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = os.path.join(BASE_DIR, 'staticfiles/')
 MEDIA_URL = os.path.join(BASE_DIR, 'media/')
 #MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
